62a5780a4218a5379b5581cc083351808e54d9c3
Hunter-sfcb <sajjad.gp.1389@gmail.com>
2025-12-05 20:59:17 +0330

    first upload

---
+++ android/app/src/main/java/com/ice/test/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ice.test;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ice.test',
+  appName: 'IcyAPP',
+  webDir: 'dist'
+};
+
+export default config;
